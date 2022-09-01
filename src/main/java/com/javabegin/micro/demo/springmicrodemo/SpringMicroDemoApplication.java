0bfd15efad920101dc0fdad21006dcf71bb85a9d
Dana <danontay@github.com>
2022-09-02 01:25:27 +0600

    first commit

---
+++ src/main/java/com/javabegin/micro/demo/springmicrodemo/SpringMicroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javabegin.micro.demo.springmicrodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javabegin/micro/demo/springmicrodemo/SpringMicroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabegin.micro.demo.springmicrodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
